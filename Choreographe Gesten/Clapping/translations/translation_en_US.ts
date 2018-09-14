51c42a73eff12a640f73ac39e110a0ad9e612ab6
carolin <carolin.hochmuth@gmail.com>
2018-09-14 14:47:39 +0200

    Choreos

---
+++ Choreographe Gesten/Clapping/translations/translation_en_US.ts
@@ -0,0 +1,46 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+    <context>
+        <name>behavior_1/behavior.xar:/Say</name>
+        <message>
+            <source>!aha! You're great sphero!</source>
+            <comment>Text</comment>
+            <translation type="obsolete">!aha! You're great sphero!</translation>
+        </message>
+        <message>
+            <source>niiiiiice, You're great sphero!</source>
+            <comment>Text</comment>
+            <translation type="obsolete">niiiiiice, You're great sphero!</translation>
+        </message>
+        <message>
+            <source>You're great, sphero!</source>
+            <comment>Text</comment>
+            <translation type="obsolete">You're great, sphero!</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Sphero, you're great!</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Sphero, you're great!</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Timeline/behavior_layer1/keyframe1/Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <source>haha! You're great sphero!</source>
+            <comment>Text</comment>
+            <translation type="obsolete">haha! You're great sphero!</translation>
+        </message>
+        <message>
+            <source>!aha! You're great sphero!</source>
+            <comment>Text</comment>
+            <translation type="obsolete">!aha! You're great sphero!</translation>
+        </message>
+    </context>
+</TS>
