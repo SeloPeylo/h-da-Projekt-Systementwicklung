bbcae92e6062c963ed904117ee36088733b66250
carolin <carolin.hochmuth@gmail.com>
2018-09-17 11:58:24 +0200

    akt.

--- Choreographe Gesten/Gestenportfolio/translations/translation_en_US.ts
+++ Choreographe Gesten/Gestenportfolio/translations/translation_en_US.ts
@@ -2,6 +2,15 @@
 <!DOCTYPE TS>
 <TS version="2.1" language="en_US">
     <context>
+        <name>behavior_1/behavior.xar:/Miss You</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>I miss you Spheero</source>
+            <comment>Text</comment>
+            <translation type="unfinished">I miss you Spheero</translation>
+        </message>
+    </context>
+    <context>
         <name>behavior_1/behavior.xar:/Say</name>
         <message>
             <source>!aha! You're great sphero!</source>
@@ -35,6 +44,15 @@
         </message>
     </context>
     <context>
+        <name>behavior_1/behavior.xar:/Say (2)</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>I am looking for the red ball</source>
+            <comment>Text</comment>
+            <translation type="unfinished">I am looking for the red ball</translation>
+        </message>
+    </context>
+    <context>
         <name>behavior_1/behavior.xar:/Timeline/behavior_layer1/keyframe1/Say</name>
         <message>
             <source>Hello</source>
@@ -50,6 +68,15 @@
             <source>!aha! You're great sphero!</source>
             <comment>Text</comment>
             <translation type="obsolete">!aha! You're great sphero!</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Too far</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>It is Dangerous to go alone Spheero</source>
+            <comment>Text</comment>
+            <translation type="unfinished">It is Dangerous to go alone Spheero</translation>
         </message>
     </context>
 </TS>
