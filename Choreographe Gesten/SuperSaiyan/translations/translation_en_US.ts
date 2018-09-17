968ca3e517255c596be12317bf3c9df7b9d0f722
Joerg Tran <joergtran@Joergs-MacBook-Pro.local>
2018-09-17 14:59:04 +0200

    SuperSaijan

---
+++ Choreographe Gesten/SuperSaiyan/translations/translation_en_US.ts
@@ -0,0 +1,27 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+    <context>
+        <name>behavior_1/behavior.xar:/Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <source>mmmmmmm?</source>
+            <comment>Text</comment>
+            <translation type="obsolete">mmmmmmm?</translation>
+        </message>
+        <message>
+            <source>Saiajin</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Saiajin</translation>
+        </message>
+        <message>
+            <source>aaaaaaaaaaa</source>
+            <comment>Text</comment>
+            <translation type="obsolete">aaaaaaaaaaa</translation>
+        </message>
+    </context>
+</TS>
