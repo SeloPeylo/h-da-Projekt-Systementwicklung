b50b39468b59f02e36bc09d777263f7d38aa2a26
Anil Alsac <anil.alsac@stud.h-da.degit config --global user.name Anil>
2018-09-17 14:40:05 +0200

    pyhton choreo box

--- resources/RedBallTrackingAA.py
+++ resources/RedBallTrackingAA.py
@@ -1,122 +1,93 @@
-# -*- encoding: UTF-8 -*-
-
-"""
-This example shows how to use ALTracker with red ball.
-"""
-
-import time
-import argparse
-from naoqi import ALProxy
-
-NAO_IP = "141.100.47.205"
-
 def getxStatus(xt,xold):
-    tts = ALProxy("ALTextToSpeech", NAO_IP, 9559)
     if xt<xold:
-        print "Right"
-     
-        tts.say("Right")
-    elif xt>xold:
-         print "Left"
-         tts.say("Left")
        
-def main(IP, PORT, ballSize):
+     
+        self.tts.say("Right")
+    elif xt>xold:
+        
+         self.tts.say("Left")
+         
+    return     
+         
+class MyClass(GeneratedClass):
+    def __init__(self):
+        GeneratedClass.__init__(self)
+        
+    def onInput_onStop(self):
+        self.onUnload() #it is recommended to reuse the clean-up as the box is stopped
+        self.onStopped() #activate the output of the box
+        
+    def onLoad(self):
+        #put initialization code here
+        self.motion=self.session().service( "ALMotion" )
+        self.posture=self.session().service( "ALRobotPosture" )
+        self.tracker=self.session().service( "ALTracker" )
+        self.tts=self.session().service( "ALTextToSpeech" )
+        self.alife=self.session().service( "ALAutonomousLife" )
+      
+        pass
 
-    print "Connecting to", IP, "with port", PORT
-    motion = ALProxy("ALMotion", IP, PORT)
-    posture = ALProxy("ALRobotPosture", IP, PORT)
-    tracker = ALProxy("ALTracker", IP, PORT)
-    tts = ALProxy("ALTextToSpeech", IP, PORT)
-    alife = ALProxy("ALAutonomousLife", IP, PORT)
+    def onUnload(self):
+        #put clean-up code here
+        pass
 
-    # First, wake up
-    motion.wakeUp()
-    print "waking up"
+    def onInput_onStart(self):
+        #self.onStopped() #activate the output of the box
+        self.motion.wakeUp()
+        self.alife.setState("disabled")
+        
+        
+        self.tracker.stopTracker()
+        self.tracker.unregisterAllTargets()
+        self.tracker.toggleSearch(False)
 
-    alife.setState("disabled")
+        self.tts.say("Red Ball Detector started")
+        self.fractionMaxSpeed = 0.8
+        # Go to posture stand
+        self.posture.goToPosture("StandInit", self.fractionMaxSpeed)
 
-    tracker.stopTracker()
-    tracker.unregisterAllTargets()
-    tracker.toggleSearch(False)
+        # Add target to track.
+        self.targetName = "RedBall"
+        self.diameterOfBall = 0.06
+        self.tracker.registerTarget(self.targetName, self.diameterOfBall)
+         # set modes
+        self.mode = "Head"
+        self.tracker.setMode(self.mode)
+        # Then, start tracker.
+        self.tracker.track(self.targetName)
 
-    tts.say("Red Ball Detector started")
-
-    fractionMaxSpeed = 0.8
-    # Go to posture stand
-    posture.goToPosture("StandInit", fractionMaxSpeed)
-
-    # Add target to track.
-    targetName = "RedBall"
-    diameterOfBall = ballSize
-    tracker.registerTarget(targetName, diameterOfBall)
-    
-	
-    # set modes
-    mode = "Head"
-    tracker.setMode(mode)
-    # Then, start tracker.
-    tracker.track(targetName)
-
-    print "ALTracker successfully started, now show a red ball to robot!"
-    print "Use Ctrl+c to stop this script."
-    hasTarget = None
-    x=0
-    try:
+   
+        self.hasTarget = None
+        self.x=0
         while True:
-            #Cordinates in variables
+                #Cordinates in variables
           
-            if tracker.isNewTargetDetected():
-                tracker.pointAt("LArm", tracker.getTargetPosition(0), 0, 0.8)
+                if self.tracker.isNewTargetDetected():
+                    self.tracker.pointAt("LArm",self.tracker.getTargetPosition(0), 0, 0.8)
               
-                postion=tracker.getTargetPosition(0)
-                print "x: "+str( postion[0])+"y: " +str( postion[1])+ "z: "+str(postion[2])
-            
-                if x!=0:
-                    print"iam in the zone"
-                    getxStatus(postion[1],x)
-                    x=postion[1]
-            if tracker.isNewTargetDetected() and hasTarget == None:
-                tts.say("I have detected a red ball")
-                tracker.pointAt("LArm", tracker.getTargetPosition(0), 0, 0.8)
-                x=postion[1]
+                    self.postion=self.tracker.getTargetPosition(0)
+                    
+                    if self.x!=0:
+                        self.tts.say("IAM IN THE ZONE")
+                        self.getxStatus(self.postion[1],self.x)
+                        self.x=self.postion[1]
+                if self.tracker.isNewTargetDetected() and self.hasTarget == None:
+                    self.tts.say("I have detected a red ball")
+                    self.tracker.pointAt("LArm", self.tracker.getTargetPosition(0), 0, 0.8)
+                    self.x=self.postion[1]
                
                
-                print tracker.getTargetPosition(0)
-                posture.goToPosture("StandInit", fractionMaxSpeed)
-                hasTarget = True
+                    
+                    self.posture.goToPosture("StandInit", self.fractionMaxSpeed)
+                    self.hasTarget = True
 
         
-            if tracker.isTargetLost() and hasTarget == True:
-                tts.say("I have lost my Target!")
-                hasTarget = None
+                if  self.tracker.isTargetLost() and self.hasTarget == True:
+                    self.tts.say("I have lost my Target!")
+                    self.hasTarget = None
 
-            time.sleep(0.5)
+                    self.time.sleep(0.5)
+                self.tts.say("end of line")
+                
 
-    except KeyboardInterrupt:
-        print
-        print "Interrupted by user"
-        print "Stopping..."
-
-    # Stop tracker, go to posture Sit.
-    tracker.stopTracker()
-    tracker.unregisterAllTargets()
-    #posture.goToPosture("Sit", fractionMaxSpeed)
-    motion.rest()
-
-    print "ALTracker stopped."
-    tts.say("Red Ball Detector stopped.")
-    alife.setState("solitary")
-
-if __name__ == "__main__" :
-
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--ip", type=str, default=NAO_IP,
-                        help="Robot ip address.")
-    parser.add_argument("--port", type=int, default=9559,
-                        help="Robot port number.")
-    parser.add_argument("--ballsize", type=float, default=0.06,
-                        help="Diameter of ball.")
-
-    args = parser.parse_args()
-
-    main(args.ip, args.port, args.ballsize)
+    
