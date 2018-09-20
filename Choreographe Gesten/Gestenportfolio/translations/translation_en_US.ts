cc5510cfc1ce8dda645385fd03cf0f178c9dd262
Muhammed Selim Sinan <stmusina@stud.h-da.de>
2018-09-20 15:09:43 +0200

    Tracker update

--- Choreographe Gesten/Gestenportfolio/translations/translation_en_US.ts
+++ Choreographe Gesten/Gestenportfolio/translations/translation_en_US.ts
@@ -172,6 +172,20 @@
         </message>
     </context>
     <context>
+        <name>behavior_1/behavior.xar:/Reset_and_hear/behavior_layer1/keyframe1/Tracker/behavior_layer1/keyframe1/Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Tracker stopped</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Tracker stopped</translation>
+        </message>
+    </context>
+    <context>
         <name>behavior_1/behavior.xar:/Say</name>
         <message>
             <source>!aha! You're great sphero!</source>
